412c86fc37b6cffaaeea2fa62af46d390b528f80
Akshay-Jogarajiya <jogarajiyaakshay@gmail.com>
2026-01-10 18:19:20 +0530

    Spring Boot project set up

---
+++ backend-spring/src/test/java/com/ScreenSense/ScreenSense/AI/ScreenSenseAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ScreenSense.ScreenSense.AI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScreenSenseAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
