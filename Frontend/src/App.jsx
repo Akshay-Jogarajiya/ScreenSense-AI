783bc0aea2ca7287e57d8ea9ff0798c1201451a0
pareshbavaliya21 <bavaliyaparesh532@gmail.com>
2026-01-10 23:45:22 +0530

    initial chnages

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import LoginPage from "./auth/LoginPage";
+import SignupPage from "./auth/SignupPage";
+import Dashboard from "./componets/Dashboard";
+
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Navigate to="/dashboard" />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/signup" element={<SignupPage />} />
+      <Route path="*" element={<h2>Page Not Found</h2>} />
+    </Routes>
+  );
+}
+
+export default App;
